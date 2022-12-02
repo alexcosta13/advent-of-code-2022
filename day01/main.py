ff15804fa86111b1398421572a4b1ffa9b7439b7
alexcosta13 <alex.costasanchez@gmail.com>
2022-12-02 15:19:16 +0100

    refactor: python formatting

--- day01/main.py
+++ day01/main.py
@@ -1,7 +1,9 @@
 if __name__ == "__main__":
     with open("input.txt") as f:
         lines = f.read().strip()
-    calories = [sum([int(food) for food in elf.split("\n")]) for elf in lines.split("\n\n")]
+    calories = [
+        sum([int(food) for food in elf.split("\n")]) for elf in lines.split("\n\n")
+    ]
 
     part1 = max(calories)
     print("First part:", part1)
